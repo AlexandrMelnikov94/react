3ee71d5a63ad246486393bf4b0d44d763c62b20f
AlexandrMelnikov94 <76219280+AlexandrMelnikov94@users.noreply.github.com>
2021-11-30 13:02:10 +0300

    add alert

--- lesson4/homework/src/App.js
+++ lesson4/homework/src/App.js
@@ -1,14 +1,34 @@
 import Phone from "./Phone/Phone";
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
 
-      <Phone name = {'IPhone'} model = {5}/>
-      <Phone name = {'IPhone'} model = {6}/>
-      <Phone name = {'Samsung'} model = {'A50'}/>
-    </div>
-  );
+  state = {
+    phones: [
+      {name: 'IPhone', model: 5},
+      {name: 'IPhone', model: 6},
+      {name: 'Samsung', model: 'A50'}
+    ],
+    pageTitle: 'Warning!!! Do not touch'
+  }
+
+  alertHandler = () => {
+    alert('ALAAAAARM');
+  }
+
+  render() {
+    const phones = this.state.phones;
+
+    return (
+      <div className="App">
+        <h1 onMouseMove={this.alertHandler}>{this.state.pageTitle}</h1>
+
+        <Phone name = {phones[0].name} model = {phones[0].model} />
+        <Phone name = {phones[1].name} model = {phones[1].model} />
+        <Phone name = {phones[2].name} model = {phones[2].model} />
+      </div>
+    );
+  }
 }
 
 export default App;
